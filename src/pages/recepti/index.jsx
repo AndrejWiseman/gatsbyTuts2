cc8541afb4941fdd0eee7e8fec6d6577cddc1cea
A.M <andrej2000aa@gmail.com>
2023-03-21 10:04:45 +0100

    promena boje

---
+++ src/pages/recepti/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+
+const Recepti = () => {
+    return (
+        <Layout>
+            <h1>Ovo je stranica za recepte</h1>
+        </Layout>
+    )
+}
+
+export default Recepti
